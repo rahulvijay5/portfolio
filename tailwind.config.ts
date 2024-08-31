fa7c88ef7dbefb7b271bcaaa5827e9e724dddaef
rahulvijay5 <coolrvj5@gmail.com>
2024-09-01 02:05:37 +0530

    updated tailwind config for shadcn 2.0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -161,22 +161,6 @@ const config = {
   					transform: 'translateY(calc(-100% - var(--gap)))'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
@@ -191,8 +175,6 @@ const config = {
   			backgroundPositionSpin: 'background-position-spin 3000ms infinite alternate',
   			marquee: 'marquee var(--duration) linear infinite',
   			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		},
   		fontFamily: {
   			sans: ["var(--font-sans)", ...fontFamily.sans],
